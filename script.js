a0c76b9765587d3b7ae45ba64382b8ea2718ca1e
Anhtuanzzz <62417928+Anhtuanzzz@users.noreply.github.com>
2022-02-13 11:31:19 +0700

    Add more infomations

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 // Temporary use this file to save something I haven't used yet
 
 //  color: #2e3e61;
+
+/* 
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+*/
